9025a2ff00af24f570fc512add0a7859b3d07304
Abhinav Chavali <abhinavchavali12@gmail.com>
2021-12-14 17:02:16 -0500

    Finished line numbering

--- src/ui/editor.rs
+++ src/ui/editor.rs
@@ -98,13 +98,8 @@ impl<'a> Editor<'a> {
 fn draw_line<T: Write>(
     term: &mut Terminal<T>,
     line: &RopeSlice,
-    line_num: u64,
     x_size: u16,
 ) -> Result<(), io::ErrorKind> {
-    term.print(fg(Color::RGB(153, 153, 102))).unwrap();
-    term.print(line_num).unwrap();
-    term.print(" ").unwrap();
-    term.print(fg(Color::Reset)).unwrap();
     term.print(line).unwrap();
     term.set_cursor_to(term.x_pos, term.y_pos + 1).unwrap();
     Ok(())
@@ -139,10 +134,52 @@ impl<'a> Component for Editor<'a> {
                 draw_statusline(term, curr_buf, x).unwrap();
 
                 term.set_cursor_to(1, 1).unwrap();
+                let buff_lc = curr_buf.line_count;
+
+                let mut buf_offset = 0;
+                match buff_lc {
+                    1..=9 => {
+                        buf_offset = 1;
+                    }
+                    10..=99 => {
+                        buf_offset = 2;
+                    }
+                    100..=999 => {
+                        buf_offset = 3;
+                    }
+                    1000..=9999 => {
+                        buf_offset = 4;
+                    }
+                    _ => {
+                        buf_offset = 5;
+                    }
+                }
+
                 for line in 1..y {
-                    if curr_buf.line_count > line.into() {
+                    if buff_lc > line.into() {
+                        // Line numbers + offset
+                        term.print(fg(Color::RGB(153, 153, 102))).unwrap();
+
+                        let mut o_str = String::new();
+                        let mut o_count = buf_offset;
+                        if (1..10).contains(&line) {
+                            o_count -= 1;
+                        } else if (10..100).contains(&line) {
+                            o_count -= 2;
+                        } else if (100..1000).contains(&line) {
+                            o_count -= 3;
+                        }
+                        for _ in 0..o_count {
+                            o_str.push(' ');
+                        }
+                        term.print(o_str).unwrap();
+                        term.print(line).unwrap();
+                        term.print(fg(Color::Reset)).unwrap();
+                        term.print(" ").unwrap();
+
+                        // Render text
                         let curr_line = curr_buf.rope.line(line.into());
-                        draw_line(term, &curr_line, line.into(), x).unwrap();
+                        draw_line(term, &curr_line, x).unwrap();
                     } else {
                         term.print("~").unwrap();
                         term.set_cursor_to(term.x_pos, term.y_pos + 1).unwrap();
